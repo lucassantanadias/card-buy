f4ad03c4dfedd78fdd7b2dce6eff635f2c540d0e
lucas <lucassantanadias9@gmail.com>
2023-01-24 19:34:50 -0300

    add js

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggle() {
+  document.documentElement.classList.toggle("animated")
+}
+//document: toda a página visível do cartão
+
+//.documentElement: dentro do document existe um documento html
+
+//.classList: na lista de classes do html...
+
+//.toggle: na lista de classes do html, você vai fazer a opção de toggle (alternar)
+
+//Eu dei o seguinte comando: pega o documento, na lista de classes dele, e faça uma troca: se tiver o animated, você vai tirar, se não tiver, você vai adicionar
